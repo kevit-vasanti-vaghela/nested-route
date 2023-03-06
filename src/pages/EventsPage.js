81199de49bd8a7a5dc40acc1a055f770a852769f
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-06 16:12:17 +0530

    error handled with custom error message

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -4,6 +4,10 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
   const data = useLoaderData();
+
+  if (data.isError) {
+    return <p>{ data.message }</p>
+  }
   const events = data.events;
   return (
        <EventsList events={events} />
@@ -16,10 +20,10 @@ export default EventsPage;
 
 // separate loader funciton then added as value to loader property of route definition of page.
 export async function loader () {
-  const response = await fetch('http://localhost:8080/events');
+  const response = await fetch('http://localhost:8080/eventsd');
 
               if (!response.ok) {
-                //...
+                return { isError: true, message: 'Could not fetch data.' }
               } else {
                 
                 return response;
