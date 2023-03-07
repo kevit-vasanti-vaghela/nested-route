ba9e07b50b1614d8897b30aa8b055dbdc1b3af14
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-07 11:55:05 +0530

    form added to new event page

--- src/pages/NewEventPage.js
+++ src/pages/NewEventPage.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import EventForm from '../components/EventForm'
 
 const NewEventPage = () => {
   return (
-    <div>
-      <h1>New Event Page</h1>
-    </div>
+    <EventForm />
   )
 }
 
